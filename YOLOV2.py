0445dc84a9b6d3c9991a9f8d4a2c63a98ba2ccec
Michael Menezes <menezmic21@gmail.com>
2022-07-22 17:39:23 -0500

    min area rect for static images

--- YOLOV2.py
+++ YOLOV2.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import torch
+from time import time
 
 # Model
 #model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
@@ -31,49 +32,61 @@ def getLineLength(line):
 
     x0, y0, x1, y1 = line.flatten()
 
-    return (x1 - x0)**2 + (y1 - y0)**2
+    return pow((x1 - x0)**2 + (y1 - y0)**2, .5)
 
 
 def plot_boxes(model, results, frame):
+
     labels, cord = results
     n = len(labels)
     x_shape, y_shape = frame.shape[1], frame.shape[0]
-    bluredFrame = cv.bilateralFilter(frame, 7, 50, 50)
+    bluredFrame = cv.bilateralFilter(frame, 7, 50, 50) #blur spaces while keeping edges
+    bluredFrame = cv.cvtColor(bluredFrame, cv.COLOR_BGR2HSV)
+    tol = 10
+    threshold = cv.inRange(bluredFrame, (23-tol, 41-tol, 133-tol), (40+tol, 255, 255))
+    bluredFrame = cv.bitwise_and(bluredFrame, bluredFrame, mask=threshold)
+
     margin = 20
+
     for i in range(n):
         row = cord[i]
         # If score is less than 0.2 we avoid making a prediction.
         if row[4] < 0.2: 
             continue
+
         x1 = linearConstraint(x_shape, 0, int(row[0]*x_shape) - margin)
         y1 = linearConstraint(y_shape, 0, int(row[1]*y_shape) - margin)
         x2 = linearConstraint(x_shape, 0, int(row[2]*x_shape) + margin)
         y2 = linearConstraint(y_shape, 0, int(row[3]*y_shape) + margin)
+
         bgr = (0, 255, 0) # color of the box
         classes = model.names # Get the name of label index
         label_font = cv.FONT_HERSHEY_SIMPLEX #Font for the label.
 
         cv.rectangle(frame, (x1, y1), (x2, y2), bgr, 2) #Plot the boxes
         cv.putText(frame, "Cube", (x1, y1), label_font, 0.9, bgr, 2) #Put a label over box.; classes[labels[i]] vs "cube"
-        miniFrame = frame[y1:y2, x1:x2]
+        
+        miniFrame = frame[y1:y2, x1:x2] #getting subimage
         miniFrame = cv.cvtColor(miniFrame, cv.COLOR_BGR2GRAY)
-        # miniFrame = cv.normalize(miniFrame,  miniFrame, 0, 255, cv.NORM_MINMAX)
-        # ret, threshold = cv.threshold(miniFrame, 75, 255, cv.THRESH_BINARY_INV)
-        # miniFrame = cv.inpaint(miniFrame, threshold, 3, cv.INPAINT_TELEA)
-        miniFrame = cv.Canny(bluredFrame[y1:y2, x1:x2], 100, 200)
+
+        miniFrame = cv.Canny(bluredFrame[y1:y2, x1:x2], 100, 200) #Edge detection
         kernel = np.ones((5,5), np.uint8)
         edges = cv.dilate(miniFrame, kernel, iterations=1)
-        lsd = cv.createLineSegmentDetector(0)
-        lines = lsd.detect(edges)[0]
-        print(type(lines))
-        lines = np.array([line for line in lines if getLineLength(line) > 25])
-        print(type(lines))
-        
+
+        # lsd = cv.createLineSegmentDetector(0) #Line detection
+        # lines = lsd.detect(edges)[0]
+        # lines = np.array([line for line in lines if getLineLength(line) > (y2-y1) * 0.15])
+
+        # linearImage = np.zeros((y2-y1, x2-x1,3), np.uint8)
+        # lsd.drawSegments(linearImage, lines)
+        # linearImage = cv.cvtColor(linearImage, cv.COLOR_BGR2GRAY)
+
         #x, y, w, h = cv.boundingRect(edges)
-        contours, hierarchy = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) #Convex hull
         contours = [cv.convexHull(cnt) for cnt in contours]
         contours = sorted(contours, key=lambda x: cv.contourArea(x), reverse=True)
-        rect = cv.minAreaRect(contours[0])
+
+        rect = cv.minAreaRect(contours[0]) #min area rect
         box = cv.boxPoints(rect)
         box = np.int0(box)
 
@@ -81,24 +94,28 @@ def plot_boxes(model, results, frame):
 
         for cnt in contours:
             cv.drawContours(miniFrame, [cnt], 0, (255, 0, 255), 1)
-        
-        lsd.drawSegments(miniFrame, lines)
 
-        left = box[1]
-        right = box[3]
-        top = box[2] 
-        bottom = box[0]
+        vertexLst = sorted(box, key=lambda x: x[1], reverse=True)
 
-        cv.circle(miniFrame, left, 8, (0, 50, 255), -1)
-        cv.circle(miniFrame, right, 8, (0, 255, 255), -1)
-        cv.circle(miniFrame, top, 8, (255, 50, 0), -1)
-        cv.circle(miniFrame, bottom, 8, (255, 255, 0), -1)
+        first = vertexLst[0]
+        second = vertexLst[1]
+        third = vertexLst[2]
+        fourth = vertexLst[3]
+
+        cv.circle(miniFrame, first, 8, (255, 50, 0), -1)
+        cv.circle(miniFrame, second, 8, (255, 255, 0), -1)
+        cv.circle(miniFrame, third, 8, (255, 255, 255), -1)
+        cv.circle(miniFrame, fourth, 8, (255, 255, 255), -1)
 
         frame[y1:y2, x1:x2] = miniFrame
 
     return frame
 
+start_time = time()
 frame = plot_boxes(model, (labels, cord), frame)
+end_time = time()
+fps = 1/np.round(end_time - start_time, 3) #Measure the FPS.
+print(f"Frames Per Second : {fps}")
 
 # Results
 results.print()  
